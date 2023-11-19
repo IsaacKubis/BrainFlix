c20444ec96f990ef4f73fb5ce4541a387ff905d7
Isaac Kubis <isaackubis1@gmail.com>
2023-11-19 08:44:44 -0800

    initial upload of files

---
+++ src/components/Component/header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="header">
+            <img src="../../assets/images/BrainFlix-logo.svg" alt="BrainFlix Logo" className="header__image" />
+        </header>
+    )
+}
+
+export default Header;
