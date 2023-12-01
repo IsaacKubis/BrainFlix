f4dfd637e0abaf106793a71de66894dc69099240
Isaac Kubis <isaackubis1@gmail.com>
2023-12-01 11:13:21 -0800

    Header component done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.scss';
 import logo from "./assets/images/BrainFlix-logo.svg";
 import userPic from "./assets/images/Mohan-muruge.jpg";
 import Header from "./components/header/header";
+import uploadPic from './assets/images/icons/upload.svg';
 
 function App() {
   return (
-    <Header logo={logo} userPic={userPic} />
+    <Header logo={logo} userPic={userPic} uploadPic={uploadPic}/>
   );
 }
 
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,9 +2,11 @@
 function Header(props) {
     return (
         <header className="header">
-            <img src={props.logo} alt="BrainFlix Logo" className="header__image" />
+            <div className="header__logo-container"> 
+                <img src={props.logo} alt="BrainFlix Logo" className="header__logo" />  
+            </div>
             <input type="search" className="header__search-bar" placeholder="Search"/>
-            <button className="header__button">Upload</button>
+            <button className="header__button" type=""><img src={props.uploadPic} alt='publish img' className="header__button__img"/>Upload</button>
             <div className="header__profile-image-container">
                 <img src={props.userPic} alt="profile" className="header__profile-picture" height={40} width={40}/>
             </div>
